b108a956df98695aae18106ae44e641ea4d7d8d7
r64genji <tabe259@yahoo.com>
2026-01-28 09:25:29 +0800

    updated vite config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://abeserver.ddns.net:3000',
         changeOrigin: true,
       }
     }
